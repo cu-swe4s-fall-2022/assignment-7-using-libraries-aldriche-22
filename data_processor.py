5ea77226a826b86770863cfb4b15a5191050b48b
Emma Aldrich <aldriche22@mail.wlu.edu>
2022-10-21 22:42:08 +0000

    built function get_random_matrix to pass the unit test in test_data_processor, implemented the function in plotter.py

--- data_processor.py
+++ data_processor.py
@@ -1,10 +1,35 @@
 # remember to import your libraries!
+import numpy as np
+
+
+#TODO
+'''
+In the file data_processor.py, create a function called get_random_matrix(). This function
+should create and return an N x M matrix of random floating point numbers sampled from a uniform in
+the range of (0,1] (hint checkout numpy.random.rand()).
+Input parameters:
+a. num_rows : number of rows, integer > 0
+b. num_columns : number of columns, integer > 0
+Return 2-dimensional numpy array
+'''
+
 
 def get_random_matrix(num_rows, num_columns):
-	return None
+    '''
+    Inputs:
+    num_rows: int
+        number of rows in the matrix
+    num_columns: int
+        number of columns in the matrix
+        
+    Outputs:
+    matrix: array of dimensions defined by num_rows, num_columns
+    '''
+    matrix = np.random.rand(num_rows, num_columns)
+    return matrix
 
 def get_file_dimensions(file_name):
-	return (0,0)
+    return (0,0)
 
 def write_matrix_to_file(num_rows, num_columns, file_name):
-	return None
+    return None

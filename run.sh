bc7a963360c5ea193ff843911bf5de94f184496b
Emma Aldrich <aldriche22@mail.wlu.edu>
2022-10-26 22:57:03 +0000

    updated functional tests for plotter.py

--- run.sh
+++ run.sh
@@ -6,16 +6,14 @@ set -o pipefail
 
 python3 data_processor.py
 
-python3 plotter.py --file_name 'iris.data'
+python3 plotter_redo.py --file_name 'iris.data' 
 
-
-#test scripts with pycodestyle
+# test scripts with pycodestyle
 # pycodestyle plotter.py 
 # pycodestyle test_data_processor.py 
 
-#run unit tests
-# python3 test_data_processor.py 
-# python3 test_plotter.py
+# run unit tests
+python3 test_data_processor.py 
 
-#run functional tests
-# bash func_test.sh
+# run functional tests
+bash func_test.sh
